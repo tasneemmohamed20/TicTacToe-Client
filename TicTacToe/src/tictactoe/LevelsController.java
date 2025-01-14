d6e28b9a7c2a67b8d58aa052bf3eca16726f6014
nada gharib <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>
2025-01-14 19:39:04 +0200

    updated

--- TicTacToe/src/tictactoe/LevelsController.java
+++ TicTacToe/src/tictactoe/LevelsController.java
@@ -5,9 +5,19 @@
  */
 package tictactoe;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,6 +26,13 @@ import javafx.fxml.Initializable;
  */
 public class LevelsController implements Initializable {
 
+    @FXML
+    private Button easyBtn;
+    @FXML
+    private Button mediumBtn;
+    @FXML
+    private Button hardBtn;
+
     /**
      * Initializes the controller class.
      */
@@ -23,6 +40,31 @@ public class LevelsController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+  private void showAlert(Alert.AlertType alertType, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+    @FXML
+    private void handleHardModeNavigation(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("FXML.fxml"));
+            Parent loginRoot = loader.load();
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene loginScene = new Scene(loginRoot);
+            stage.setScene(loginScene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            showAlert(Alert.AlertType.ERROR, "Navigation Error", "Could not navigate to the Local page.");
+        }
+    }
+    }
+
     
     
-}
+
